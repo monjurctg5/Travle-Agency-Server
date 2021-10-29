f04213152615cb68316a77fb09268b14fbb56056
monjurctg5 <monjurctg5@github.com>
2021-10-29 17:36:27 +0600

    service post and get method done

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const cors = require('cors')
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
 
+const ObjectId = require('mongodb').ObjectId
+
 const app = express()
+
 const port= process.env.PORT || 5000
+
+//midleware
+
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghw3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,9 +21,24 @@ async function run() {
   try {
     await client.connect();
 
-    // const database = client.db("sample_mflix");
-    // const movies = database.collection("movies");
-    console.log("kaj kortece abar kaj kortece")
+    const database = client.db("Travelo");
+    const serviceColection = database.collection("Services");
+
+    app.get('/services',async(req,res)=>{
+      const cursor = serviceColection.find({})
+      const result = await cursor.toArray()
+      res.send(result)
+      console.log(result)
+    })
+
+
+    app.post('/services',async(req,res)=>{
+      const newService = req.body;
+      const result = await serviceColection.insertOne(newService)
+      res.send(result)
+      console.log(result)
+    })
+   
 
   
       } finally {
